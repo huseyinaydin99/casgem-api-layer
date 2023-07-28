fe03fc3af124ba617316494b69a5879380fad40a
huseyinaydin99 <huseyinaydin99@gmail.com>
2023-07-28 11:00:27 +0300

    Projenin son hali.

--- Casgem.ConsumeLayer/Controllers/DefaultController.cs
+++ Casgem.ConsumeLayer/Controllers/DefaultController.cs
@@ -3,6 +3,7 @@ using Newtonsoft.Json;
 using Casgem.DtoLayer.DTOs;
 using AutoMapper;
 using Casgem.DtoLayer.DTOs.EstateDTOs;
+using System.IO;
 
 namespace Casgem.ConsumeLayer.Controllers
 {
@@ -18,16 +19,8 @@ namespace Casgem.ConsumeLayer.Controllers
             _mapper = mapper;
         }
 
-
-        /*
         [HttpGet]
-        public async Task<IActionResult> Index()
-        {
-            return View();
-        }*/
-
-        [HttpGet]
-        public async Task<IActionResult> Index(string p = "", int price = 0, int room = 0)
+        public async Task<IActionResult> Index(string searchForEverything = "", int price = 0, int room = 0)
         {
             var client = _httpClientFactory.CreateClient();
             var responseMessage = await client.GetAsync("https://localhost:7160/api/Estate");
@@ -35,18 +28,16 @@ namespace Casgem.ConsumeLayer.Controllers
             if (responseMessage.IsSuccessStatusCode)
             {
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<List<Estate>>(jsonData);
+                var values = JsonConvert.DeserializeObject<List<Casgem.DtoLayer.DTOs.EstateDTOs.Estate>>(jsonData);
 
-                if (!string.IsNullOrEmpty(p))
+                if (!string.IsNullOrEmpty(searchForEverything))
                 {
-                    var searchString = p.ToLower();
+                    var searchString = searchForEverything.ToLower();
                     values = values.Where(y =>
                         y.City.ToLower().Contains(searchString) ||
                         y.Title.ToLower().Contains(searchString) ||
                         y.Type.ToLower().Contains(searchString)).ToList();
                 }
-
-               
 
                 if (price != 0 && room != 0)
                 {
@@ -64,35 +55,25 @@ namespace Casgem.ConsumeLayer.Controllers
                 {
                     return View(values);
                 }
-
+                string createText = "Hello and Welcome" + Environment.NewLine;
+                using (StreamWriter writer = new StreamWriter("C:\\Users\\Huseyin_Aydin\\Desktop\\text_abc.txt"))
+                {
+                    writer.WriteLine("If'e girdi. Sorun yok.");
+                }
                 return View(values);
+            }
+            using (StreamWriter writer = new StreamWriter("C:\\Users\\Huseyin_Aydin\\Desktop\\text_abc.txt"))
+            {
+                writer.WriteLine("If'e girmedi.. Sorun var.");
             }
             return View();
         }
-
-        //[HttpGet]
-        //public async Task<IActionResult> Search(string? city, string? type, int? room, string? title, int? price, string? buildYear)
-        //{
-        //    var queryString = $"?city={city}&type={type}&room={room}&title={title}&price={price}&buildYear={buildYear}";
-        //    var client = _httpClientFactory.CreateClient();
-        //    var responseMessage = await client.GetAsync($"https://localhost:7160/api/Estate/filter{queryString}");
-
-        //    if (responseMessage.IsSuccessStatusCode)
-        //    {
-        //        var jsonData = await responseMessage.Content.ReadAsStringAsync();
-        //        var values = JsonConvert.DeserializeObject<List<Estate>>(jsonData);
-        //        return PartialView("_SearchResults", values);
-        //    }
-
-        //    return PartialView("_SearchResults", new List<Estate>());
-        //}
-
 
         [HttpGet]
         public async Task<IActionResult> Result(string id = null)
         {
             var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync($"https://localhost:44314/api/Estate/{id}");
+            var responseMessage = await client.GetAsync($"https://localhost:7160/api/Estate/{id}");
             if (responseMessage.IsSuccessStatusCode)
             {
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
@@ -101,21 +82,5 @@ namespace Casgem.ConsumeLayer.Controllers
             }
             return View();
         }
-        
-
-        /*
-        [HttpGet]
-        public async Task<IActionResult> Result(string id = null)
-        {
-            var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync($"https://localhost:44314/api/Estate/{id}");
-            if (responseMessage.IsSuccessStatusCode)
-            {
-                var jsonData = await responseMessage.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<Estate>(jsonData);
-                return View(values);
-            }
-            return View();
-        }*/
     }
 }
